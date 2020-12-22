fbd80d498fa9c964ee1551fd40ba29eaa2d666f3
AP-Atul <atulpatare99@gmail.com>
2020-12-22 11:23:10 +0530

    removed unused codes

--- test/snr_test.py
+++ test/snr_test.py
@@ -19,16 +19,9 @@ t = FastWaveletTransform(WAVELET_NAME)
 outputFileName = os.path.join(OUTPUT_DIR, "_" + WAVELET_NAME + ".wav")
 noiseRatios = list()
 
-with soundfile.SoundFile(outputFileName, "w", samplerate=rate, channels=1) as of:
+with soundfile.SoundFile(outputFileName, "w", samplerate=rate, channels=info.channels) as of:
     start = time()
     for block in soundfile.blocks(INPUT_FILE, int(rate * info.duration * 0.10)):  # reading 10 % of duration
-
-        # processing only single channel
-        if block.ndim > 1:
-            block = block.T
-            block = block[0]
-        else:
-            block = block.flatten()
 
         coefficients = t.waveDec(block)
 
